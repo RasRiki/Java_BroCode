d7abb140ddd5f39b4b81cbb0ddf9bd70371bc439
Nome <alessandrorusso217@gmail.com>
2025-06-23 05:49:30 -0700

    D

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -85,15 +85,47 @@ public class GamePanel extends JPanel implements Runnable{
         }
 
         //PALLA CON RACCHETTA 1
-        if(ball.x == PADDLE_WIDTH){
+        /*if(ball.x == PADDLE_WIDTH){
             if(ball.y >= p1.y && ball.y <= (p1.y + PADDLE_HEIGHT)){
+                ball.xVelocity++;
+
+
                 ball.setXDirection(ball.xVelocity * -1);
             }
+        }*/
+
+        if (ball.x + BALL_DIAMETER >= p1.x &&
+                ball.x <= p1.x + PADDLE_WIDTH &&
+                ball.y + BALL_DIAMETER >= p1.y &&
+                ball.y <= p1.y + PADDLE_HEIGHT){
+
+            ball.xVelocity++;
+            ball.yVelocity++;
+
+            ball.setXDirection(ball.xVelocity * -1);
+
         }
+
         //PALLA CON RACCHETTA 2
-        if(ball.x == (GAME_WIDTH - PADDLE_WIDTH))
-            if(ball.y >= p2.y && ball.y <= (p2.y + PADDLE_HEIGHT))
+        /*if(ball.x + BALL_DIAMETER == (GAME_WIDTH - PADDLE_WIDTH))
+            if(ball.y >= p2.y && ball.y <= (p2.y + PADDLE_HEIGHT)) {
+                ball.xVelocity++;
+                ball.yVelocity++;
+
                 ball.setXDirection(ball.xVelocity * -1);
+            }*/
+        if (ball.x + BALL_DIAMETER >= p2.x &&
+                ball.x <= p2.x + PADDLE_WIDTH &&
+                ball.y + BALL_DIAMETER >= p2.y &&
+                ball.y <= p2.y + PADDLE_HEIGHT){
+
+            ball.xVelocity++;
+            ball.yVelocity++;
+
+            ball.setXDirection(ball.xVelocity * -1);
+
+        }
+
     }
 
     public void run(){
